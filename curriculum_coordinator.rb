5d38380cf68e215391cf97b78319f4cb49e1b32b
Steven G. Harms <steven@devbootcamp.com>
2014-07-23 06:51:07 -0700

    Status Quo: curriculum coordinator

---
+++ curriculum_coordinator.rb
@@ -0,0 +1,9 @@
+class CurriculumCoordinator
+  # Coordinating/curating teacher resources.
+
+  # Approving curriculum experiment proposals.
+
+  # Overseeing curriculum experiments and managing the documentation and communication of these experiments.
+
+  # Communicating curriculum gaps and issues and updates to role with a domain of curriculum
+end
