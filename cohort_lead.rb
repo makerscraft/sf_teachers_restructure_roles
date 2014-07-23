d6942979a5dfd3b1dda70d900455065345472b23
Steven G. Harms <steven@devbootcamp.com>
2014-07-23 06:49:51 -0700

    Status Quo: cohort lead

---
+++ cohort_lead.rb
@@ -0,0 +1,11 @@
+class CohortLead
+  # Stewarding an individual cohort throughout the three phases
+
+  # Nurturing cohort culture
+
+  # Facilitating discussions between students and staff
+
+  # Educating students on DBC cultural values and reinforcing these values when they are compromised
+
+  # Coordinating with SF - Diversity Steward and SF - Engineering Empathy Facilitator on matters of Engineering Empathy and diversity
+end
