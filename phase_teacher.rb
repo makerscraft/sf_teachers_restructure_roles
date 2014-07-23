bf476ce31c77735eb8faec647810bc61db6776df
Steven G. Harms <steven@devbootcamp.com>
2014-07-23 07:15:55 -0700

    Restore PhaseTeacher

---
+++ phase_teacher.rb
@@ -0,0 +1,9 @@
+class PhaseTeacher < Teacher
+  # Implementing daily continuity for the Phase
+
+  # Coordinating Floating Teachers and curating resources for Phase
+
+  # Liaising with Cohort Lead
+
+  # Liaising w/ Future & Previous Phase Lead
+end
