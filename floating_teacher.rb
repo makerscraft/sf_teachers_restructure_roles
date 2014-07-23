882b37ed76a9eb786eb6d17fad74a3425b2a0f03
Steven G. Harms <steven@devbootcamp.com>
2014-07-23 06:42:31 -0700

    Status Quo: FloatingTeacher

---
+++ floating_teacher.rb
@@ -0,0 +1,11 @@
+class FloatingTeacher
+  # Backing up cohort SF - Cohort Lead Teacher
+
+  # Facilitating cross cohort collaboration
+
+  # Reviewing code
+
+  # Paring with students
+
+  # Consulting with student groups
+end
