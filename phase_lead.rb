18c95098ae5da7a4b01d015087f3da354284bcda
Steven G. Harms <steven@devbootcamp.com>
2014-07-23 06:42:31 -0700

    Status Quo: PhaseLead

---
+++ phase_lead.rb
@@ -0,0 +1,19 @@
+class PhaseLead
+  # Implementing daily continuity
+  
+  # Designing and Managing processes for effective student learning
+  
+  # Managing student access to teaching staff during core hours
+   
+  # Proposing and running curriculum experiments for the phase
+  
+  # Leading Lectures
+  
+  # Managing support for students on the floor
+  
+  # Assessing and communicating student progress throughout the phase and at the end of a phase. Communicating to students and Cohort Lead.
+  
+  # Coordinating teaching staff and curating resources
+  
+  # Liaising with Cohort Lead
+end
