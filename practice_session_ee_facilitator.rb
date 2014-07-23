692b759ba40c9ca79e2064bb5f34ea1249d53699
Steven G. Harms <steven@devbootcamp.com>
2014-07-23 06:51:50 -0700

    Status Quo: practice session ee facilitator

---
+++ practice_session_ee_facilitator.rb
@@ -0,0 +1,8 @@
+class PracticeSessionEEFacilitator
+  # facilitating EE practice sessions
+
+  # maintaining personal practice in accordance with EE practice curriculum
+
+  # maintaining annual attendance of the deep dive sessions of the practice sessions being conducted
+end
+
