8dc16681eda3db165c3255d300c18ee348d517f5
Steven G. Harms <steven@devbootcamp.com>
2014-07-23 07:33:56 -0700

    AssistantTeacher#supporting, #assessing

--- assistant_teacher.rb
+++ assistant_teacher.rb
@@ -1,3 +1,4 @@
 class AssistantTeacher < Teacher
-  # supporting teacher
+  # Supporting Teachers through code review, floor support, break out sessions and other requested activities
+  # Assessing and communicating student progress throughout the phase and at the end of a phase.
 end
